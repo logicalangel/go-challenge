7240345739f2e815acef22fe5706a2a5f8964a10
Amir Rastegar <rastegar.amir3@gmail.com>
2022-04-19 21:03:59 +0430

    implt controllers and functions for input data

--- cmd/uss/main.go
+++ cmd/uss/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"arman-task/internals/controller"
+	"fmt"
+)
 
+func main() {
+	fmt.Println("---- Add new user ----")
+	for {
+		controller.GetUserAndSegmention()
+	}
 }
