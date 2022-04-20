a9cc1a422fe15eee08d1c2fd64ff8ca70ae375f9
Amir Rastegar <rastegar.amir3@gmail.com>
2022-04-21 03:33:15 +0430

    implmt and check grpc server - make project cleanup and repository injectable - implmt min-heap for keeping user create time

--- cmd/es/main.go
+++ cmd/es/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"arman-task/internal/controller"
+	"arman-task/internal/repository"
+	"arman-task/internal/services/grpc"
+	"arman-task/internal/usecase"
+	"fmt"
+)
 
+func main() {
+	segmentMap := repository.NewSegmentMap()
+	userHeap := repository.NewUserTtlHeap()
+
+	go grpc.NewServer(&userHeap, &segmentMap)
+	go usecase.RunGarbageCollector(segmentMap, userHeap)
+
+	fmt.Println("---- get segment estimate ----")
+	for {
+		controller.GetSegmentEstimate(segmentMap)
+	}
 }
